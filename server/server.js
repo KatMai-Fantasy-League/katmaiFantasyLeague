32e910ca99565a03dc157e8a79c4db1b4f2341f0
leilani <lherna05@gmail.com>
2022-09-27 16:13:32 -0700

    Imported all dbs, set up auth/routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,29 @@
 const path = require('path');
+
+
+// Database imports
+const db = require("./models/pgWrapper");
+const tokenDB = require("./models/tokenModel")(db);
+const userDB = require("./models/userModel")(db);
+
+// OAuth imports
+const oAuthService = require("./auth/tokenService")(userDB, tokenDB);
+const oAuth2Server = require("node-oauth2-server");
+
 const express = require('express');
+
 const app = express();
+app.oauth = oAuth2Server({
+    model: oAuthService,
+    grants: ["password"],
+    debug: true,
+});
+const testAPIService = require("./test/testAPIService.js");
+const testAPIRoutes = require("./test/testAPIRoutes.js")(
+    express.Router(),
+    app,
+    testAPIService
+);
 const authenticator = require("./auth/authenticator")(userDB);
 const routes = require("./auth/routes")(
     express.Router(),
@@ -27,23 +50,23 @@ app.get('/', (req, res) => {
 const myBracketRouter = require('./routes/myBracketRoute');
 const resultsBracketRouter = require('./routes/resultBracketRoute'); 
 
-//SIGN UP/LOGIN Routes 
-app.post('/signup', loginControllers.createUser, (req, res) => {
-  console.log('WE HAVE ENTERED SIGN UP ROUTE HANDLER');
-  return res.status(200);  //redirect to login
-});
+// //SIGN UP/LOGIN Routes 
+// app.post('/signup', loginControllers.createUser, (req, res) => {
+//   console.log('WE HAVE ENTERED SIGN UP ROUTE HANDLER');
+//   return res.status(200);  //redirect to login
+// });
 
-// this is the endpoint for logging in
-app.post('/login', loginControllers.verifyUser, loginControllers.createToken, (req, res) => {
-  console.log('ENTERED LOGIN ROUTE HANDLER');
-  return res.status(200); //redirect to homepage
-});
+// // this is the endpoint for logging in
+// app.post('/login', loginControllers.verifyUser, loginControllers.createToken, (req, res) => {
+//   console.log('ENTERED LOGIN ROUTE HANDLER');
+//   return res.status(200); //redirect to homepage
+// });
 
-// this is a template for authentication on any of our homepage routes 
-app.get('/afterLogin', loginControllers.checkForToken, loginControllers.verifyToken, (req, res) => {
-  console.log('ENTERED AFTER LOGIN ROUTE HANDLER');
-  res.status(200);
-});
+// // this is a template for authentication on any of our homepage routes 
+// app.get('/afterLogin', loginControllers.checkForToken, loginControllers.verifyToken, (req, res) => {
+//   console.log('ENTERED AFTER LOGIN ROUTE HANDLER');
+//   res.status(200);
+// });
 
 app.use('/myBracket', myBracketRouter);
 
@@ -70,4 +93,4 @@ app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
 
-module.exports = app, router;
+module.exports = { app, router };
