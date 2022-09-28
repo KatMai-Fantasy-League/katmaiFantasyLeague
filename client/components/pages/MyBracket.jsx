6948b28a974a348ef4ce02bdb1548e3269f585ab
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-27 19:51:03 -0700

    updated useEffect, get data from server

--- client/components/pages/MyBracket.jsx
+++ client/components/pages/MyBracket.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BracketOutline from '../BracketOutline';
 import BracketForm from '../BracketForm';
+import { convertFromSQL } from '../../utils/sqlConvertFrom';
 
 function MyBracket(props) {
+  useEffect(async () => {
+    const getBears = async () => {
+      const response = await fetch('api/myBracket');
+      const data = await response.json();
+      return data;
+    };
+
+    const getBracket = async () => {
+      const response = await fetch('api/bears');
+      const data = await response.json();
+      return data;
+    };
+
+    const bracketData = await getBracket();
+    const bearsList = await getBears();
+
+    setMyBracket(convertFromSQL(bearsList, bracketData));
+  }, []);
+
   const { myBracket, setMyBracket } = props;
 
   return (
