aed592e46ec6a01f2ac726d949b75fec1e2c4537
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2022-09-28 10:40:17 -0700

    Added Google Sign In button to Login Page

--- client/components/pages/LoginPage.jsx
+++ client/components/pages/LoginPage.jsx
@@ -1,6 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import bigBear from '../../images/bigbear.png';
+// import jwt_decode from 'jwt-decode';
 
 function LoginPage() {
-  return <div>Login Page</div>;
+
+  // const [ user, setUser ] = useState({});
+
+  // function called after we choose a google account to sign in with
+  function handleCallbackResponse(response) {
+    // decode the response JWT and can use the data google provides for us
+    // const userObject = jwt_decode(response.credential);
+    // console.log(userObject);
+    // setUser(userObject);
+    // document.getElementById("signInDiv").hidden = true;
+    // document.querySelector('.login-heading').textContent = `Welcome ${userObject.name}`
+    fetch('/login', {
+      Method: 'POST',
+      Headers: { 'Content-Type': 'application/json'},
+      Body: response
+    })
+  }
+
+  // function handleSignOut() {
+  //   setUser({});
+  //   document.getElementById("signInDiv").hidden = false;
+  //   document.querySelector('.login-heading').textContent = `Login or Sign Up with your Google account below:`
+  // }
+
+  useEffect(() => {
+    // global google - google object coming from script in HTML
+    google.accounts.id.initialize({
+      client_id: "599848243160-fh9f74vresv83v5g9oec13q73vnq1mnv.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    // renders the google login button
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large", shape: "pill"}
+    )
+  }, []);
+
+  return (
+    <>
+    <h1 className="login-heading">Login or Sign Up with your Google account below:</h1>
+    <img src={bigBear} alt="Chubby brown bear sitting in grass"></img>
+    <div id="signInDiv"></div>
+    {/* { Object.keys(user).length !== 0 &&
+      <button onClick = { () => handleSignOut() }>Sign Out</button>
+    } */}
+    </>
+  )
 }
 export default LoginPage;
